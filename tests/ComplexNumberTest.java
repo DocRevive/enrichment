27a070a1e4a3e7387d09109046d25dd5035c0719
Revive <53571319+DocRevive@users.noreply.github.com>
2022-05-01 23:53:38 -0400

    style

--- tests/ComplexNumberTest.java
+++ tests/ComplexNumberTest.java
@@ -1,14 +1,14 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ComplexNumberTest {
-    private ComplexNumber cn;
-    private ComplexNumber cn2;
+    private static ComplexNumber cn;
+    private static ComplexNumber cn2;
 
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         cn = new ComplexNumber(2.0, -3.0);
         cn2 = new ComplexNumber(-5.0, -3.0);
     }
@@ -19,54 +19,54 @@ public class ComplexNumberTest {
     }
 
     @Test
-    void subtract() {
+    public void subtract() {
         assertEquals("7.0+0.0i", cn.subtract(cn2).toString());
     }
 
     @Test
-    void multiply() {
+    public void multiply() {
         assertEquals("-19.0+9.0i", cn.multiply(cn2).toString());
     }
 
     @Test
-    void divide() {
+    public void divide() {
         ComplexNumber quotient = cn.divide(cn2);
         assertEquals(-0.02941, quotient.getA(), 1e-5);
         assertEquals(0.61765, quotient.getB(), 1e-5);
     }
 
     @Test
-    void negative() {
+    public void negative() {
         assertEquals("-2.0+3.0i", cn.negative().toString());
         assertEquals("5.0+3.0i", cn2.negative().toString());
     }
 
     @Test
-    void absoluteValue() {
+    public void absoluteValue() {
         assertEquals(3.6055, cn.absoluteValue(), 1e-4);
         assertEquals(5.831, cn2.absoluteValue(), 1e-3);
     }
 
     @Test
-    void argument() {
+    public void argument() {
         assertEquals(-0.98279, cn.argument(), 1e-5);
         assertEquals(-2.60117, cn2.argument(), 1e-5);
     }
 
     @Test
-    void conjugate() {
+    public void conjugate() {
         assertEquals("2.0+3.0i", cn.conjugate().toString());
         assertEquals("-5.0+3.0i", cn2.conjugate().toString());
     }
 
     @Test
-    void square() {
+    public void square() {
         assertEquals("-5.0-12.0i", cn.square().toString());
         assertEquals("16.0+30.0i", cn2.square().toString());
     }
 
     @Test
-    void testToString() {
+    public void testToString() {
         assertEquals("2.0-3.0i", cn.toString());
         assertEquals("-5.0-3.0i", cn2.toString());
     }
--- tests/GridRectCounterTest.java
+++ tests/GridRectCounterTest.java
@@ -1,29 +1,29 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GridRectCounterTest {
-    private GridRectCounter rect1;
-    private GridRectCounter rect2;
-    private GridRectCounter rect3;
+public class GridRectCounterTest {
+    private static GridRectCounter rect1;
+    private static GridRectCounter rect2;
+    private static GridRectCounter rect3;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    public static void setUp() {
         rect1 = new GridRectCounter(1, 3);
         rect2 = new GridRectCounter(2, 2);
         rect3 = new GridRectCounter(5, 4);
     }
 
     @Test
-    void countRectangles() {
+    public void countRectangles() {
         assertEquals(6, rect1.countRectangles());
         assertEquals(9, rect2.countRectangles());
         assertEquals(150, rect3.countRectangles());
     }
 
     @Test
-    void countSquares() {
+    public void countSquares() {
         assertEquals(3, rect1.countSquares());
         assertEquals(5, rect2.countSquares());
         assertEquals(40, rect3.countSquares());
